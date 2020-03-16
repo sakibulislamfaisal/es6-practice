c82725c8fae484da6a0ad1ab42dd9cc56fe004db
foyshal <fr7199732@gmail.com>
2020-03-16 23:16:13 +0600

    array operation

---
+++ array slice-splice-join.js
@@ -0,0 +1,10 @@
+const arr = [2,3,5,6,7,8,9,4,3];
+// const sliceValue = arr.slice(3,7);
+// console.log(sliceValue);
+
+// const removed = arr.splice(2,4);
+// console.log(removed);
+// console.log(arr);
+
+const joinArray = arr.join(" / ");
+console.log(joinArray);
