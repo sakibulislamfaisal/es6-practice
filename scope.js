8be572e1428198fd2cd4a585834c749cf9c21407
foyshal <fr7199732@gmail.com>
2020-03-16 22:37:11 +0600

    scope and global variable

---
+++ scope.js
@@ -0,0 +1,11 @@
+function sum(n1,n2){
+     //const total = n1 + n2;
+     let total = n1 + n2 ;
+     console.log(total);
+     return total ;
+}
+
+var today = "friday";
+const  result = sum(2,4);
+console.log(result);
+console.log(today);
