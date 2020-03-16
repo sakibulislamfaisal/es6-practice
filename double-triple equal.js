8d572f95b1adcf3f3f26297cfd76a158f3b6eb6b
foyshal <fr7199732@gmail.com>
2020-03-16 21:00:11 +0600

    double vs triple equal sign

---
+++ double-triple equal.js
@@ -0,0 +1,17 @@
+// const n1 = 3;
+// const n2 = "3";
+
+// var n1=1;
+// var n2=true;
+
+var n1 = 0;
+var n2 = false;
+
+if(n1===n2){
+     console.log("value is equal");
+}
+else{
+     console.log("value is not equal");
+}
+//double equal means just check the value if value is equal then it return true
+//triple equal means it check the value and data types if two condition is true then it will return true
