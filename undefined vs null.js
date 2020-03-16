2d9f44b86c39593d0ddf657fc2040b678cf1b702
foyshal <fr7199732@gmail.com>
2020-03-16 20:41:43 +0600

    undefined vs null

---
+++ undefined vs null.js
@@ -0,0 +1,33 @@
+//undefined check
+
+//1.
+// let name ;
+// console.log(name);
+
+//2.
+ 
+// function add(m1,m2){
+//      console.log(m1,m2);
+// }
+// var result = add(12,33);
+// console.log(result);
+
+//3.
+// function add(m1,m2){
+//       console.log(m1,m2);
+//     }
+//     var result = add(12);
+//     console.log(result);
+
+//4.
+
+// var person = { name : "faisal" , age:20}
+// console.log(person.phone);
+
+//5.
+// var arr = [1,2,34,45,5,3];
+// console.log(arr[8]);
+
+//6.
+let fun = undefined;
+console.log(fun);
